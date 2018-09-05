88b682abafbb560572c1ef4359207766fc0beb33
Aamer Sohail <aamersohail.09@gmail.com>
2018-09-05 18:53:07 +0530

    Add files via upload

---
+++ Try_your_luck/script.js
@@ -0,0 +1,28 @@
+var randomNumber = getRandomNumber(10);
+var guess;
+var guessCount = 0;
+var correctGuess = false;
+
+
+function getRandomNumber(upper){
+var num = Math.floor(Math.random()*upper)+1;
+return num;
+}
+
+alert('!!Try your luck !! \n\n\ Here is a game which decides your Luck \n\n\ There are three people in jail it is said that who ever wins is freed from jail');
+
+while(true){ 
+   guessCount +=1;
+guess = prompt('whom are you expecting among 1 to 10?');
+
+ if(parseInt(guess) === randomNumber){
+         correctGuess=true;
+        break;
+      }    
+} 
+
+document.write('<h1 style=background-color:slateblue;> You guessed the Right person!</h1>');
+document.write(' It took you '+ guessCount+' tries to guess the Correct person '+ randomNumber);
+
+
+
